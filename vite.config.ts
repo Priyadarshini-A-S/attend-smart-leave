13557fd93b7b8e28d928d360bf7cc99d1c52328d
Priyadarshini A S <priyadarshini.as2024cse@sece.ac.in>
2025-10-07 15:53:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/attend-smart-leave/" : "/", // IMPORTANT for GitHub Pages subpath
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: mode === "production" ? "/attend-smart-leave/" : "/", // must match repo name
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: "::",
+    port: 8080,
   },
 }));
 
